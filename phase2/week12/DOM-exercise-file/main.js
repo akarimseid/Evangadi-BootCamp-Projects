0abe821e0dd441e0c7551a2e0ae9a0e5fd00a471
akarimseid <abdulkerimseid99@gmail.com>
2026-08-30 17:53:31 +0300

    added a bonus question for dom and event also solve some questions on dom

--- phase2/week12/DOM-exercise-file/main.js
+++ phase2/week12/DOM-exercise-file/main.js
@@ -1,3 +1,102 @@
 //  your solutions start here
-//  make sure to connect your main.js file with your html 
+//  make sure to connect your main.js file with your html
 // happy coding 🧑‍💻
+
+/*
+Question 1: The following three questions are based on the two paragraphs under the
+section which says "For Question 1" in the index.html file.
+1.1 Select the element with an id of "sample1".
+1.2 Print the element itself on the console upon page refresh.
+1.3 Print the content of the element on the console upon page refresh.
+Hint: Use the "textContent" property to select the content
+*/
+//1.1
+const sampleElement = document.getElementById("sample1");
+
+//2.2
+console.log(sampleElement);
+
+//3.3
+console.log(sampleElement.textContent);
+
+/*
+Question 2: The following questions are based on the HTML code found under the
+section labeled "For question 2".
+
+2.1 Select the element with an ID of "techCompanies" and display 7it on your
+console. (Do not use "querySelector" for this question)
+*/
+
+const techCompanies = document.getElementById("techCompanies");
+// console.log(techCompanies);
+
+// 2.2 Use "querySelector" to select the element with an ID of "techCompanies"
+// and display it on your console.
+
+const techCompanies2 = document.querySelector("#techCompanies")
+console.log(techCompanies2);
+
+// 2.3 How many tech companies are listed under the ul element with an id of "techCompanies"? Use "querySelectorAll" to count the total.
+
+// selecting all li elements under the id of techCompanies
+ const myUl = document.getElementById("techCompanies");
+ const totalCount = myUl.querySelectorAll("li").length;
+ console.log(totalCount);
+
+// 2.4 Select all elements with a class name of "red" and display them on the console. Use both "querySelectorAll" and "getElementByClass"
+
+const redElementsList = document.querySelectorAll(".red");
+console.log(`querySelectorAll results `,redElementsList);
+redElementsList.forEach(element => console.log(element));
+
+//using getElementByClassname it returns collection (live)
+const redElementsCollection = document.getElementsByClassName("red");
+
+//displaying using standard  for loop
+for (let i = 0; i <redElementsCollection.length; i++){
+    console.log(redElementsCollection[i]);
+}
+ //using for of loop
+for (const element in redElementsCollection){
+    console.log(element);
+}
+
+
+// 2.5 Create a new li HTML element with a content of "Facebook" and display it on console
+
+
+const newLi = document.createElement("li");
+newLi.textContent = "Facebook";
+  console.log(newLi);
+
+  // 2.6 Give the newly created element a class of "blue" using JavaScript
+newLi.className = "blue";
+
+// 2.7 Append the newly created element next to the the "Sony" li element
+const techContainer = document.getElementById("techCompanies");
+techContainer.appendChild(newLi);
+
+//  2.8 How many of the tech companies are labeled blue? Find the result using JavaScript and display the result inside the "blueCompanies" div.
+
+/*
+Question 3:
+Change the background color of the page to light-blue (#99ecff) when clicked on the text
+that says "Yes". If there is a background color set already, change it to none when clicked
+on "No"
+Hint: First, write two functions to alter the backgroundColor of the page. One to add a
+background color, another to remove. Then, select the "yesBackground" or
+"noBackground" element and bind the selected element with the click event. Finally,
+attach the function you wrote to alter the background color when the respective element
+is clicked on.
+*/
+
+/*
+Question 4:
+A form with two text fields is provided under the section which says "For question 4".
+Write a JavaScript code which takes the values of the two fields, checks if they are
+number values and calculate the sum of the two numbers.
+1. Display the result on the console
+2. Display the result underneath the form
+3. If any of the numbers provided is not a number, display a message that says
+"Please enter numerical values only" underneath the form
+*/
