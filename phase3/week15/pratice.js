740b36a5467ade2a1689415b00a073994e73e489
akarimseid <abdulkerimseid99@gmail.com>
2026-09-13 11:09:57 +0300

    resources and exercies of week15

---
+++ phase3/week15/pratice.js
@@ -0,0 +1,15 @@
+// const os = require('os');
+// const osS = os.platform();
+// console.log(osS);
+// console.log(process.arch);
+
+// const fs = require("fs");
+
+// fs.readFile("app.js", "utf8" , (err, data) => {
+// if (err) {
+// console.error(err);
+// return;
+// }
+// console.log(data);
+// });
+
